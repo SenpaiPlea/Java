684916bcbd5796cac9e79be84c4c220ed9db2eef
Sen <Mikef2163@gmail.com>
2025-05-17 18:14:13 -0400

    Push two

---
+++ ProjectTwo/src/packageVariables/ClassVariables.java
@@ -0,0 +1,48 @@
+
+
+package packageVariables;
+
+ 
+
+public class ClassVariables {
+
+ 
+
+ public static void main(String[] args) {
+
+ // TODO Auto-generated method stub
+
+ int firstNumb = 5;
+
+ int secondNumb = 10;
+
+ 
+
+ double myDouble = 5.98;
+
+ char myChar = 'a';
+
+ boolean myBool = true;
+
+ 
+
+ System.out.println("My first number is: " + firstNumb);
+
+ System.out.println(secondNumb);
+
+ System.out.println(myDouble);
+
+ System.out.println(myChar);
+
+ System.out.println(myBool);
+
+ 
+
+ }
+
+ 
+
+}
+
+ 
+
